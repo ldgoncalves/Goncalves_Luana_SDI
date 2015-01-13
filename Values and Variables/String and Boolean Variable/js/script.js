c0c77e3ecf6f4e7c9a433d47c6c85a0ed8b8a647
luluphotog <ldgoncalves@fullsail.edu>
2015-01-13 17:18:00 -0500

    String and Booleans exercises

---
+++ Values and Variables/String and Boolean Variable/js/script.js
@@ -0,0 +1,9 @@
+// Variables of Different Types, Numbers, Booleans and Strings
+
+var age = 38; // Number variable
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Luana";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor';
