5b236501bf570141b69050ca0c003bcab0d3796f
luluphotog <ldgoncalves@fullsail.edu>
2015-01-15 14:51:01 -0500

    Assignment Operators

---
+++ Expressions/Assignment Operators/js/script.js
@@ -0,0 +1,17 @@
+//Expressions - Assignment Operators
+
+var a = 3;
+a ++; //a = a + 1 the same with  a += 1 all operators
+a--;
+console.log(a);
+
+
+
+
+
+//a *= 4; //a = a * 4
+
+//a /= 4; //a = a / 4
+
+//a -= 4; //a = a - 4
+
