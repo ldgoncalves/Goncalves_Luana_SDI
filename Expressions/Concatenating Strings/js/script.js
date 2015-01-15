0c51c3f3855282937da7e8a71ff83355c1d9d6b5
luluphotog <ldgoncalves@fullsail.edu>
2015-01-14 23:43:15 -0500

    Completed Arrays, order of operations, modulo
    and concatenating, casting, arithmetic operators

---
+++ Expressions/Concatenating Strings/js/script.js
@@ -0,0 +1,12 @@
+//Expressions - Concatenating
+
+//var firstName = "Luana";
+//var lastName = "Goncalves";
+//var fullName = firstName + " " + lastName;
+//console.log(fullName);
+
+var a = 6;// when something is a string it concatenates. without quotes it becomes a number.
+var b = 7;
+
+var result = a + b;
+console.log(result);
