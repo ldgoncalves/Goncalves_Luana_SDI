09d165fa835ef79a87c16c0ce47afd9d1fe8f817
luluphotog <ldgoncalves@fullsail.edu>
2015-01-15 20:47:47 -0500

    added condition
    added a condition to figure out how USD the user will need

--- Goncalves_Luana_Expressions_Assignment/js/script.js
+++ Goncalves_Luana_Expressions_Assignment/js/script.js
@@ -10,7 +10,7 @@ Expressions Assignment
 
 //Calculating the bank's percentage markup of a currency's exchange rate you want to purchase in US dollars.
 
- alert("We are calculating the bank's percentage markup of a currency's exchange rate you want to purchase in USD. Also we will compare how much USD you will need to purchase a certain currency."); //introduction of the calculator
+ alert("We are calculating the bank's percentage markup of a currency's exchange rate you want to purchase in USD. Also we will compare how much USD you will need to purchase a certain currency with the bank rate."); //introduction of the calculator
 
  var bankExchange = prompt("Please enter the bank's exchange rate:"); //for user to input bank rate of the currency they are planning to purchase
 console.log (bankExchange);
@@ -24,5 +24,20 @@ console.log(marketExchange);
 
  //Tested out to make sure the prompts and the calculation works. Used 2.75 for bank rate and 2.64 for market rate.
 
+ //Now we are going to calculate how much the user will need to purchase in USD
  var amount = prompt ("Now we will calculate how much USD you will need. Please enter desired foreign currency you need:");
+console.log(amount);
 
+ var currency = prompt("Is the foreign currency more expensive than the USD? \nYes or No");
+
+ if (currency === "yes") {
+  var total1 = amount * bankExchange;
+  console.log(total1);
+  alert("You will need $" + total1);
+ }
+else
+ {
+  var total2 = amount / bankExchange;
+  console.log(total2);
+  alert("You will need $" + parseInt(total2));
+ }
