67e4678a0c7df7a41f25511088ad803e4b28e9aa
luluphotog <ldgoncalves@fullsail.edu>
2015-01-15 19:47:08 -0500

    prompted user for currency info

--- Goncalves_Luana_Expressions_Assignment/js/script.js
+++ Goncalves_Luana_Expressions_Assignment/js/script.js
@@ -7,3 +7,18 @@ Expressions Assignment
  */
 
 //alert("Testing to see if the JS file is attached to the html.");
+
+//Calculating the bank's percentage markup of a currency's exchange rate you want to purchase in US dollars.
+
+ alert("We are calculating the bank's percentage markup of a currency's exchange rate you want to purchase in USD. Also we will compare how much USD you will need to purchase a certain currency.");
+
+ var bankExchange = prompt("Please enter the bank's exchange rate:");
+console.log (bankExchange);
+ var marketExchange = prompt("Please enter the market exchange:");
+console.log(marketExchange);
+ var percentage = (bankExchange - marketExchange)/marketExchange * 100;
+
+  alert("The percentage markup of your exchange rate is  " + parseInt(percentage) + " percent");
+
+ var amount = prompt ("Now we will calculate how much USD you will need. Please enter desired foreign currency you need:");
+
