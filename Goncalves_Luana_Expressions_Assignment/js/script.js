c80e68c3d3a075cbdc718f7d948c0e985c706da5
luluphotog <ldgoncalves@fullsail.edu>
2015-01-15 19:58:35 -0500

    Added comments and tested prompts
    Added comments to my code, explaining in detail. Also tested out the
    prompts that I have created.

--- Goncalves_Luana_Expressions_Assignment/js/script.js
+++ Goncalves_Luana_Expressions_Assignment/js/script.js
@@ -10,15 +10,19 @@ Expressions Assignment
 
 //Calculating the bank's percentage markup of a currency's exchange rate you want to purchase in US dollars.
 
- alert("We are calculating the bank's percentage markup of a currency's exchange rate you want to purchase in USD. Also we will compare how much USD you will need to purchase a certain currency.");
+ alert("We are calculating the bank's percentage markup of a currency's exchange rate you want to purchase in USD. Also we will compare how much USD you will need to purchase a certain currency."); //introduction of the calculator
 
- var bankExchange = prompt("Please enter the bank's exchange rate:");
+ var bankExchange = prompt("Please enter the bank's exchange rate:"); //for user to input bank rate of the currency they are planning to purchase
 console.log (bankExchange);
- var marketExchange = prompt("Please enter the market exchange:");
-console.log(marketExchange);
- var percentage = (bankExchange - marketExchange)/marketExchange * 100;
 
-  alert("The percentage markup of your exchange rate is  " + parseInt(percentage) + " percent");
+ var marketExchange = prompt("Please enter the market exchange:");//for user to input market rate of the currency they are planning to purchase
+console.log(marketExchange);
+
+ var percentage = (bankExchange - marketExchange)/marketExchange * 100;//the equation to figure the out the percentage markup
+
+  alert("The percentage markup of your exchange rate is  " + parseInt(percentage) + "%");//user's percentage mark up result
+
+ //Tested out to make sure the prompts and the calculation works. Used 2.75 for bank rate and 2.64 for market rate.
 
  var amount = prompt ("Now we will calculate how much USD you will need. Please enter desired foreign currency you need:");
 
