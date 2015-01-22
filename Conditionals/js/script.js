00ecc35ffe31bea672439b5f5937e941b0a1fcf8
luluphotog <ldgoncalves@fullsail.edu>
2015-01-21 23:34:42 -0500

    Conditional logic
    practicing

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -25,3 +25,39 @@ if(kidHeight + sneakerLift > minHeight){
     // code performed if condition is true
     console.log("You can ride the coaster!");
 }
+
+//Conditional Logic - With an Expression
+
+var kidHeight = 52;
+var minHeight = 48
+
+// if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight <= minHeight){
+    // code performed if condition is true
+    console.log("You can ride the coaster!");
+}else{
+    //code performed if condition is false
+    console.log("Sorry kid, you've go some growing to do first!");
+}
+
+//Conditional Logic - else if
+
+var kidHeight = 52;
+var minHeight = 48;
+var wParentHeight = 45; //the height of the kid with the parent
+
+// if the child is old enough, print to the console "you can ride!"
+//if the kid is over 48 inches in height
+
+if(kidHeight > minHeight){
+    // you can ride!
+    console.log("You can ride the coaster!");
+}else if(kidHeight > wParentHeight){
+    //you can ride with a parent present
+    console.log("You can ride, but only with a parent present.");
+}else{
+    //sorry you have growing to do
+    console.log("Sorry kid, you've go some growing to do first!");
+}
