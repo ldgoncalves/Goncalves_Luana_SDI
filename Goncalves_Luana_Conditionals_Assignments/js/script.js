0357a87bc0fc33aed79419d0dd605228aadc4c67
luluphotog <ldgoncalves@fullsail.edu>
2015-01-24 21:50:57 -0500

    Editing, ternary and logical operator
    fixed the prompts that need to alert if nothing is inputed, added
    ternary and added logical operators as well as comments.

--- Goncalves_Luana_Conditionals_Assignments/js/script.js
+++ Goncalves_Luana_Conditionals_Assignments/js/script.js
@@ -23,7 +23,7 @@ Conditionals Assignment
  //This is only if there's no input and the user will be prompted again. Tested to see if works and it does.
  if (months == ""){
   alert("You forgot to input something!");
-  var months = prompt("How many months do you have until your next trip?")
+  var months = prompt("How many months do you have until your next trip?");
  }
 
  var savings = prompt("How much are you willing to save per month?");
@@ -37,12 +37,28 @@ Conditionals Assignment
 
  var result = budget/months;
 
-
- if (result >= savings){
-  console.log("You have more saving to do.")
+ if (result > savings){
+  console.log("You have more saving to do. You should be saving $" + parseInt(result) + " a month.");
+  alert("You have more saving to do. You should be saving $" + parseInt(result) + " a month.");
  }else if (result <= savings){
-  console.log("You are on the right track for your next trip!")
+  console.log("You are on the right track for your next trip!");
+  alert("You are on the right track for your next trip!");
  }
 
  /*Tested out the prompts for now. Added 500 as budget, 6 as months, and 25 for savings. Printed out "You have more saving to do." To get the print out "You are on the right track for your next trip!", I just changed the savings to 85.*/
+
+//Now we will prompt the user with the least savings and ask if they would like to calculate how many months they need to extend with their current savings a month.
+
+ var help;
+
+ help = (result > savings)? prompt("Would you like to keep the your savings and calculate how many months you need to extend? \n Please enter Yes or No") : alert("Have fun in " + months + " months!");
+
+ if (help == "yes" && "Yes"){
+  var extension = budget/savings - months;
+  console.log("You have to save for an additional " + extension + " months.");
+  alert("You have to save for an additional " + extension + " months.");
+ }else if (help == "no" && "No"){
+  console.log("Good luck in your savings!");
+  alert("Good luck in your savings!");
+ }
 
