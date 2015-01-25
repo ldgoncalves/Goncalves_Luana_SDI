7750720e18d1b5ea2bf399098fbdad448868e0a9
luluphotog <ldgoncalves@fullsail.edu>
2015-01-24 20:27:56 -0500

    Travel Calculator
    Added comments and assigned 3 variables to prompts

--- Goncalves_Luana_Conditionals_Assignments/js/script.js
+++ Goncalves_Luana_Conditionals_Assignments/js/script.js
@@ -8,3 +8,14 @@ Conditionals Assignment
 
 //alert("Testing to see if the JS file is attached to the html.");
 
+//This is a calculator that will determine how much you need to save a month to meet their travel budget goals. Initially the user will be prompted and will be asked their travel budget, how many months they to save and how much they are willing to save. Finally, the calculator will compare if the saving amount is too little or good enough to save and will calculate how much they would need to save.
+
+ alert("This a your Travel Budget Calculator! We will determine how much you need to save for your next trip.")
+
+ var budget = prompt("What is your travel budget for your next trip?");
+ var months = prompt("How many months do you have until your next trip?");
+ var saving = prompt("How much are you willing to save?");
+
+ //From here we will divide 
+
+ var
