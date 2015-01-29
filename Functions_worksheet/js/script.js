e3b58bfe7a8c40e234c5081cdd81355136f0a0b7
luluphotog <ldgoncalves@fullsail.edu>
2015-01-29 14:05:23 -0500

    Comments
    Added to comments to circumference exercise

--- Functions_worksheet/js/script.js
+++ Functions_worksheet/js/script.js
@@ -8,16 +8,17 @@ Functions Worksheet
 
 //alert("Testing to see if the JS file is attached to the html.");
 
- //Circumference
+//Circumference
 
  //Calculate the circumference of a circle
 //Formula is C = 2 * 3.14 * radius
 
- var total = calcCircle(3.14,10);
+ var total = calcCircle(3.14,10); //argument - establishing pie and radius variable
 
- function calcCircle (pie, r){
+ function calcCircle (pie, r){ //parameters
   var circumference = 2 * pie * r;
   return circumference;
  }
+ //the result to be printed out. 
  console.log("The circumference of the circle is " + total);
 
