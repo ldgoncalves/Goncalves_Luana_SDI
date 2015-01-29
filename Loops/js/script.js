2cd5ce94695a9546b2d3cc59ce3e240880bc4321
luluphotog <ldgoncalves@fullsail.edu>
2015-01-29 14:48:10 -0500

    Loops
    Screencasted while loops, do while loops, and for loops

---
+++ Loops/js/script.js
@@ -0,0 +1,31 @@
+//Loops
+
+//While Loops
+
+var b = 10;//setups the index
+
+//this is an if statement and is going to be used in how to make this in a while loop
+/*if ( b > 0){
+ console.log(b);
+}*/
+
+while ( b > 0){//checks the condition
+ console.log(b + " kegs on the wall");
+ b--;//increments or decrements the index
+}
+
+//Do While Loops
+
+var c = 10;
+
+do{
+ console.log(c + " kegs on the wall")
+ c--;
+}
+while(c > 0);
+
+//For Loops
+
+for (var i = 10; i >0; i--){
+ console.log(i + " kegs on the wall")
+} //shorthand version
