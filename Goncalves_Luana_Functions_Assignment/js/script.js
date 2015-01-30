1dd473570f1d307cadd0aa14ba803a983e0a5ed7
luluphotog <ldgoncalves@fullsail.edu>
2015-01-29 19:16:58 -0500

    User prompts
    created user prompts and added comments

--- Goncalves_Luana_Functions_Assignment/js/script.js
+++ Goncalves_Luana_Functions_Assignment/js/script.js
@@ -7,3 +7,13 @@ Functions Assignment
  */
 
 //alert("Testing to see if the JS file is attached to the html.");
+
+ //This is a simple event budget calculator to determine how much money the user needs to save for event.
+
+//After the calculation is done for the budget, the user will be prompted if the event is for fundraising. If yes, the function will calculate approximately how much each guest needs to donate.
+
+ //Let's prompt the user with three questions first.
+
+ var guest = prompt("How many guests will attend your event?");
+ var budget = prompt("How much are you willing to spend per guest?");
+ var months = prompt("How many months you have until event?");
